b26d39b12c4ac6f9f395b0a6ad3250199d1b9cae
oreo2990 <knnt5323@gmail.com>
2021-02-22 15:14:10 +0900

    useRouterの使用

---
+++ pages/[mitsumori].js
@@ -0,0 +1,13 @@
+import { useRouter} from "next/router";
+
+export default function mitumori1(){
+    const router = useRouter();
+    console.log(router);
+    console.log(router.query);
+    return(
+        <div>
+            <h1>{router.query.mitsumori}の詳細です</h1>
+        </div>
+    )
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        
-      </Head>
-
-      <main>
-        <h1>
-         
-        </h1>
-      </main>
-
-      <footer>
-        
-      </footer>
+        <h1>GenMitsu</h1>  
     </div>
   )
 }
