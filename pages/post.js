fe5cd17eb81fd279665bca2bae852914bd6a51be
oreo2990 <knnt5323@gmail.com>
2021-02-22 17:24:32 +0900

    POST一覧の個別ページへの遷移作成

--- pages/post.js
+++ pages/post.js
@@ -1,19 +1,22 @@
+import Link from "next/link";
+
 export default function index({posts}){
     return(
     <div>
         <h1>POST一覧</h1>
         <ul>
         {posts.map((post) => {
-          return <li key={post.id}>{post.title}</li>;
+          return (
+            <li key={post.id}>
+                <Link href={`/posts/${post.id}`}>
+                    <a>{post.title}</a>
+                </Link>
+            </li>);
         })}
       </ul>
     </div>
     );
 }
-
-
-
-
 
 
 export async function getServerSideProps() {
