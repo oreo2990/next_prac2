b610c297197d2ce0755ea2c1d4c644f8d256eabb
oreo2990 <knnt5323@gmail.com>
2021-02-22 15:34:05 +0900

    見積書4の追加

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-        <h1>GenMitsu</h1>  
+        <h1>GenMitsu</h1> 
+        <ul>
+          <li><Link href="/見積書1"><a>見積書1</a></Link></li>
+          <li><Link href="/見積書2"><a>見積書2</a></Link></li>
+          <li><Link href="/見積書3"><a>見積書3</a></Link></li>
+          <li><Link href="/見積書4"><a>見積書4</a></Link></li>
+        </ul> 
     </div>
   )
 }
