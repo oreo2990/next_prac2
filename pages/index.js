3ab207782616e1560efe30a7e72bd876603c5779
oreo2990 <knnt5323@gmail.com>
2021-02-22 16:06:09 +0900

    見積書一覧の追記

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
         <h1>GenMitsu</h1> 
+        <h2>見積書一覧</h2>
         <ul>
           <li><Link href="/見積書1"><a>見積書1</a></Link></li>
           <li><Link href="/見積書2"><a>見積書2</a></Link></li>
@@ -12,6 +13,11 @@ export default function Home() {
           <li><Link href="/見積書5"><a>見積書5</a></Link></li>
           <li><Link href="/見積書6"><a>見積書6</a></Link></li>
         </ul> 
+
+        <br/>
+
+       
+
     </div>
   )
 }
